f2a8ddebbaad11f8724c11ebc8775192316bda8b
PradoAllan <allanprado.it@gmail.com>
2025-01-09 11:40:38 -0300

    refactor: :recycle: Cadet and Roles APIView classes

--- cadetDjangoModel/cadet/urls.py
+++ cadetDjangoModel/cadet/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 
-from .views import cadetView, RolesView
+# Class APIView para melhor personalização:
+from .views import CadetListCreateAPIView, CadetRetrieveUpdateDeleteAPIView, RolesListCreateAPIView, RolesRetrieveUpdateDeleteAPIView
 
 urlpatterns = [
-    path('cadets/', cadetView, name="cadet"),
-    path('roles/', RolesView, name="roles")
+    path('cadets/', CadetListCreateAPIView.as_view(), name='cadet-list'),
+    path('cadets/<pk>/', CadetRetrieveUpdateDeleteAPIView.as_view(), name='cadet'),
+    path('roles/', RolesListCreateAPIView.as_view(), name='roles-list'),
+    path('roles/<pk>/', RolesRetrieveUpdateDeleteAPIView.as_view(), name='roles')
 ]
+
+# Com esse código, foi usado uma funcção api_view. Funciona bem, mas geralmente, queremos criar uma classe APIView 
+# para manipularmos melhor os métodos e personalizar da maneira que queremos os endpoints.
+# from .views import cadetView, RolesView
+
+# urlpatterns = [
+#     path('cadets/', cadetView, name="cadet"),
+#     path('roles/', RolesView, name="roles")
+# ]
--- cadetDjangoModel/cadet/views.py
+++ cadetDjangoModel/cadet/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from django.http import HttpResponse
@@ -10,46 +10,77 @@ from rest_framework import status
 from .models import Cadet, Roles
 from .serializers import CadetSerializer, RolesSerializer
 
-@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
-def cadetView(request):
-    if request.method == 'GET':
-        cadet = Cadet.objects.all()
-        serializer = CadetSerializer(cadet, many=True)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
-        #return HttpResponse(serializer.data)
-    elif request.method == 'POST':
-        serializer = CadetSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        validated_data = serializer.validated_data
-        serializer.create(validated_data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    elif request.method == 'PUT':
-        pass
-    elif request.method == 'PATCH':
-        pass
-    elif request.method == 'DELETE':
+class   CadetListCreateAPIView(APIView):
+    def get(self, request):
         pass
 
+    def post(self, request):
+        pass
 
-@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
-def RolesView(request):
-    if request.method == 'GET':
-        roles = Roles.objects.all()
-        serializer = RolesSerializer(roles, many=True)
+class   CadetRetrieveUpdateDeleteAPIView(APIView):
+    def get(self, request, pk):
+        pass
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
-        serializer = RolesSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        validated_data = serializer.validated_data
-        serializer.create(validated_data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    elif request.method == 'PUT':
+    def put(self, request, pk):
         pass
-    elif request.method == 'PATCH':
+
+    def patch(self, request, pk):
         pass
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk):
         pass
+
+class   RolesListCreateAPIView(APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+class   RolesRetrieveUpdateDeleteAPIView(APIView):
+    def get(self, request, pk):
+        pass
+
+    def put(self, request, pk):
+        pass
+
+    def patch(self, request, pk):
+        pass
+
+    def delete(self, request, pk):
+        pass
+
+
+
+# @api_view(['GET', 'POST'])
+# def cadetView(request):
+#     if request.method == 'GET':
+#         cadet = Cadet.objects.all()
+#         serializer = CadetSerializer(cadet, many=True)
+
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#         #return HttpResponse(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = CadetSerializer(data=request.data)
+#         if not serializer.is_valid():
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         validated_data = serializer.validated_data
+#         serializer.create(validated_data)
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+# @api_view(['GET', 'POST'])
+# def RolesView(request):
+#     if request.method == 'GET':
+#         roles = Roles.objects.all()
+#         serializer = RolesSerializer(roles, many=True)
+
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#     elif request.method == 'POST':
+#         serializer = RolesSerializer(data=request.data)
+#         if not serializer.is_valid():
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         validated_data = serializer.validated_data
+#         serializer.create(validated_data)
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
